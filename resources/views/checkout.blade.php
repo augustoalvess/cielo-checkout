f6cd415f934555c93290d45b6842cdd08a802f80
Augusto Alves da Silva <augustoalvess@hotmail.com>
2021-10-19 08:33:16 -0300

    Ajustando

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -16,7 +16,7 @@
         @endif
 
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-7">
                 <div class="card">
                     <div class="card-body">
                         <form action="/checkout/finalizar" method="POST" id="checkoutForm" class="form-horizontal">
